3cd55566d35090fe79fde7316ec303d82706e66a
mittalji567 <amitmittal20001015@gmail.com>
2020-05-25 10:57:28 +0530

    Added-Comments

--- designs.js
+++ designs.js
@@ -1,24 +1,32 @@
 // When size is submitted by the user, call makeGrid()
 
+// Here I've used some of the javascript functions to get elements picked by id's to call events.
+// Also I've used preventDefault() so that the default things not happen.
+
 document.getElementById('sizePicker').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // When the data is submitted the grid mount function is called
+    // When the data is submitted the grid mount function is called.
+    // Defined some variables for height, width and color of grid.
+
     const height = document.getElementById('inputHeight').value;
     const width = document.getElementById('inputWidth').value;
     const color = document.getElementById('pixelCanvas');
     makeGrid(height, width, color);
   });
 
-function makeGrid(a, b, c) {
-  // Your code goes here!
-  c.innerHTML = '';
+// This makeGrid() function will take 3 arguments and make the grid on the basis of that.
 
-  for (let m = 0; m < a; m++) {
-      var row = c.insertRow(m);
-      for (let k = 0; k < b; k++) {
+function makeGrid(height, width, color) {
+  // Your code goes here!
+  color.innerHTML = '';
+
+  for (let m = 0; m < height; m++) {
+      var row = color.insertRow(m);
+      for (let k = 0; k < width; k++) {
         let cell = row.insertCell(k);
         cell.addEventListener('click', function(eve) {
+          // Changing the color of the cell after click.
           eve.target.style.backgroundColor = document.getElementById('colorPicker').value;
         });
       }
